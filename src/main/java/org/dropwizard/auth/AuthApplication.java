4606158cc20bc278c2824ea912030dee2352a68b
Josh <surettej@bu.edu>
2018-11-18 13:06:49 -0500

    adding in all auth test endpoints and registered filters

--- src/main/java/org/dropwizard/auth/AuthApplication.java
+++ src/main/java/org/dropwizard/auth/AuthApplication.java
@@ -1,8 +1,16 @@
 package org.dropwizard.auth;
 
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.dropwizard.auth.core.DropwizardAuthenticator;
+import org.dropwizard.auth.core.DropwizardAuthorizer;
+import org.dropwizard.auth.core.User;
+import org.dropwizard.auth.resource.AuthenticatedResource;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 public class AuthApplication extends Application<AuthConfiguration> {
 
@@ -23,7 +31,22 @@ public class AuthApplication extends Application<AuthConfiguration> {
     @Override
     public void run(final AuthConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+
+        BasicCredentialAuthFilter<User> authFilterBuilder =
+                new BasicCredentialAuthFilter.Builder<User>()
+                .setAuthenticator(new DropwizardAuthenticator())
+                .setAuthorizer(new DropwizardAuthorizer())
+                .setRealm("A REALM")
+                .buildAuthFilter();
+
+        environment.jersey().register(new AuthDynamicFeature(authFilterBuilder));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
+
+        // Allows @Auth annotation to inject custom principals into resource
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+
+        // Registering resources
+        environment.jersey().register(new AuthenticatedResource());
     }
 
 }
---
+++ src/main/java/org/dropwizard/auth/resource/AuthenticatedResource.java
@@ -0,0 +1,49 @@
+package org.dropwizard.auth.resource;
+
+import com.google.common.base.Optional;
+import io.dropwizard.auth.Auth;
+import org.dropwizard.auth.core.User;
+
+import javax.annotation.security.DenyAll;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path(AuthenticatedResource.AUTHENTICATED_PATH)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class AuthenticatedResource {
+
+    public static final String AUTHENTICATED_PATH  = "/auth";
+
+    @Path("/anyone")
+    @GET
+    @PermitAll
+    public User isAnyone(@Auth User user) {
+        return user;
+    }
+
+    @Path("/role")
+    @GET
+    @RolesAllowed("ADMIN")
+    public User isAdmin(@Auth User user) {
+        return user;
+    }
+
+    @Path("/deny")
+    @GET
+    @DenyAll
+    public User isDenied(@Auth User user) {
+        return user;
+    }
+
+    @Path("/optional")
+    @GET
+    public Optional<User> isOptionalAuth(@Auth Optional<User> user) {
+        return user.isPresent() ? user : Optional.<User>absent();
+    }
+}
